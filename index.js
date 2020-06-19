6608ed911617537fcc14054e8c6c8998299952d0
Eric Birdsall <eric@qualia.com>
2020-06-19 02:58:38 -0700

    add timer

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ if (quiz) {
   let random = Math.floor(Math.random() * (400 * 365));
   date = baseDate.add(random, 'days').format('YYYY-MM-DD')
   console.log(date);
+  let start = new Date();
   let answer = getWeekdayFromInput();
-  console.log('answer', answer)
+  let end = new Date();
+  console.log(`Answered in ${moment(end).diff(start) / 1000} seconds`);
 }
 Doomsday(date);

ec69b1dadfc82e7597c627b35042141eb10ce6ee
Eric Birdsall <eric@qualia.com>
2020-06-20 03:19:10 -0700

    check answers on quiz

--- get_weekday_from_input.js
+++ get_weekday_from_input.js
@@ -1,5 +1,10 @@
 import readline from 'readline-sync';
+import {numberToWeekday} from './constants.js';
+
+const line = '~~~~~~~~~~~~';
 
 export default function getWeekdayFromInput() {
+  let options = Object.keys(numberToWeekday).map(key => `\x1b[36m${key}\x1b[0m  ${numberToWeekday[key]}`);
+  console.log(Array.prototype.concat(line, ...options, line).join('\n'));
   return readline.question('What weekday is this?\n');
 }
--- index.js
+++ index.js
@@ -2,9 +2,12 @@ import yargs from 'yargs';
 import moment from 'moment';
 import Doomsday from './doomsday.js';
 
-import getWeekdayFromInput from './get_weekday_from_input.js'
+import getWeekdayFromInput from './get_weekday_from_input.js';
+import {numberToWeekday} from './constants.js';
+import {getWeekdayForDate} from './doomsday.js';
 
-yargs.boolean('quiz');
+yargs.boolean('quiz')
+     .boolean('explain');
 
 let date = yargs.argv._[0];
 const quiz = yargs.argv.quiz;
@@ -15,8 +18,15 @@ if (quiz) {
   date = baseDate.add(random, 'days').format('YYYY-MM-DD')
   console.log(date);
   let start = new Date();
-  let answer = getWeekdayFromInput();
+  let answer = numberToWeekday[getWeekdayFromInput()];
   let end = new Date();
   console.log(`Answered in ${moment(end).diff(start) / 1000} seconds`);
+  let weekday = getWeekdayForDate(date);
+
+  if (answer === weekday) {
+    console.log(`You were right! ${date} is a ${weekday}`);
+  } else {
+    console.log(`You were wrong! ${date} is a ${weekday}`);
+  }
 }
-Doomsday(date);
+// Doomsday(date);
